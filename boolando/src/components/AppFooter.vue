c5e16c70c6c29878eaa1e099f8432fdb1f3593c7
MarioBroi <inoix16@gmail.com>
2024-03-21 14:37:31 +0000

    edit AppFooter template Add template from the previous exercise

--- boolando/src/components/AppFooter.vue
+++ boolando/src/components/AppFooter.vue
@@ -8,7 +8,58 @@ export default {
 
 <template>
 
-    <h2>footer</h2>
+    <footer class="site-footer">
+
+        <div class="container d-flex">
+
+            <div class="company">
+
+                Boolando s.r.l.
+
+                <div class="info">
+
+                    <a href="#">Informazioni legali </a>
+
+                    <a href="#"> Informativa sulla pricacy </a>
+
+                    <a href="#"> Diritto di recesso</a>
+
+                </div>
+
+            </div>
+            <!-- /company info -->
+
+            <div class="socials">
+                Trovaci anche su
+
+                <div class="socials-icons">
+
+                    <a href="#">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-facebook-f" aria-hidden="true"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-pinterest" aria-hidden="true"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-youtube-play" aria-hidden="true"></i>
+                    </a>
+
+                </div>
+
+            </div>
+            <!-- /socials -->
+
+        </div>
+        <!-- /footer container -->
+
+    </footer>
+    <!-- /.site-footer -->
 
 </template>
 
