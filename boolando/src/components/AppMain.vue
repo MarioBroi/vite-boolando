ab91874d38813f9e17a1fe4fc11a53cca544ab2e
MarioBroi <inoix16@gmail.com>
2024-03-21 14:36:37 +0000

    edit AppMain template Add template from the previous exercise

--- boolando/src/components/AppMain.vue
+++ boolando/src/components/AppMain.vue
@@ -8,7 +8,194 @@ export default {
 
 <template>
 
-    <h3>main</h3>
+    <main class="site-main">
+
+        <section class="container-img d-flex flex-center products">
+
+            <div class="product">
+
+                <img src="./assets/img/1.webp" alt="" class="main-image">
+                <img src="./assets/img/1b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Levi's</span>
+                    <span><a href="#" class="link">relaxed fit tee unisex</a></span>
+                    <span class="new-price">14,99€</span>
+                    <del class="old-price">29.99€</del>
+
+                    <div class="products-badge">
+
+                        <div class="discount">-50%</div>
+                        <div class="eco">Sostenibilitá</div>
+
+                    </div>
+                    <!-- /product badge -->
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 1 -->
+
+            <div class="product">
+
+                <img src="./assets/img/2.webp" alt="" class="main-image">
+                <img src="./assets/img/2b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Guess</span>
+                    <span><a href="#" class="link">roses tee</a></span>
+                    <span class="new-price">20,99€</span>
+                    <del class="old-price">29.99€</del>
+
+                    <div class="products-badge">
+
+                        <div class="discount">-30%</div>
+
+                    </div>
+                    <!-- /product badge -->
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 2 -->
+
+            <div class="product">
+
+                <img src="./assets/img/3.webp" alt="" class="main-image">
+                <img src="./assets/img/4b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Come Zucchero Filato</span>
+                    <span><a href="#" class="link">voglia di colori pastello</a></span>
+                    <span class="new-price">129,99€</span>
+                    <del class="old-price">184.99€</del>
+
+                    <div class="products-badge">
+
+                        <div class="discount">-30%</div>
+
+                    </div>
+                    <!-- /product badge -->
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 3 -->
+
+            <div class="product">
+
+                <img src="./assets/img/4.webp" alt="" class="main-image">
+                <img src="./assets/img/4b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Levi's</span>
+                    <span><a href="#" class="link">tee unisex</a></span>
+                    <span class="new-price">14,99€</span>
+                    <del class="old-price">29.99€</del>
+
+                    <div class="products-badge">
+
+                        <div class="discount">-50%</div>
+                        <div class="eco">Sostenibilitá</div>
+
+                    </div>
+                    <!-- /product badge -->
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 4 -->
+
+            <div class="product">
+
+                <img src="./assets/img/5.webp" alt="" class="main-image">
+                <img src="./assets/img/5b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Maya Deluxe</span>
+                    <span><a href="#" class="link">stripe bodice</a></span>
+                    <span class="new-price">99,99€</span>
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 5 -->
+
+            <div class="product">
+
+                <img src="./assets/img/6.webp" alt="" class="main-image">
+                <img src="./assets/img/6b.webp" alt="" class="second-image">
+                <!-- /products images -->
+
+                <div class="details">
+
+                    <span class="brand">Esprit</span>
+                    <span><a href="#" class="link">maglione - black</a></span>
+                    <span class="new-price">29,99€</span>
+
+                    <div class="products-badge">
+
+                        <div class="eco">Sostenibilitá</div>
+
+                    </div>
+                    <!-- /product badge -->
+
+                    <div class="favourite">
+                        &hearts;
+                    </div>
+                    <!-- /heart -->
+
+                </div>
+                <!-- /details -->
+
+            </div>
+            <!-- /product 6 -->
+
+        </section>
+        <!-- /section container product -->
+
+    </main>
+    <!-- /.site-main -->
 
 </template>
 
