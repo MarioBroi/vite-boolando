88a755a64a8b0deb7d52911e2a8e91c686946a01
MarioBroi <inoix16@gmail.com>
2024-03-21 14:34:23 +0000

    edit AppHeader template Add template from the previous exercise

--- boolando/src/components/AppHeader.vue
+++ boolando/src/components/AppHeader.vue
@@ -14,7 +14,44 @@ export default {
 
 <template>
 
-    <h1>Header</h1>
+    <header class="site-header">
+
+        <nav class="container d-flex">
+
+            <div class="d-flex">
+
+                <a href="#" class="padding">Donna</a>
+                <a href="#" class="padding">Uomo</a>
+                <a href="#" class="padding">Bambini</a>
+
+            </div>
+            <!-- /gender selection -->
+
+            <!-- <img src="img/boolean-logo.png" alt="" class="logo"> -->
+            <!-- /image logo Booleaner -->
+
+            <div>
+
+                <a href="#">
+                    <i class="fa fa-user"></i>
+                </a>
+
+                <a href="#">
+                    <i class="fa fa-heart-o"></i>
+                </a>
+
+                <a href="#">
+                    <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                </a>
+
+            </div>
+            <!-- /header icons -->
+
+        </nav>
+        <!-- /nav container -->
+
+    </header>
+    <!-- /.site-header -->
 
 </template>
 
